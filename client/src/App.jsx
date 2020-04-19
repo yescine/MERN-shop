1d92ff45d4c7951852b04d3d826507234c757cf0
bouch <yassine.bouchoucha@cocentris.com>
2020-04-19 16:18:07 +0100

    establish react client

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import AppNavbar from './components/AppNavbar'
+
+function App() {
+  return (
+    <div className="App">
+      <AppNavbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/AppNavbar.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react'
+
+import {
+   Collapse,
+   Navbar,
+   NavbarToggler,
+   NavbarBrand,
+   Nav,
+   NavItem,
+   NavLink,
+   UncontrolledDropdown,
+   DropdownToggle,
+   DropdownMenu,
+   DropdownItem,
+   NavbarText,
+   Container
+ } from 'reactstrap';
+
+function AppNavbar() {
+   const [isOpen,setIsOpen] = useState(false)
+
+   const toogle=()=>{
+      setIsOpen(!isOpen)
+   }
+
+   return (
+      <div>
+         <Navbar color="dark" dark expand="sm" className="mb-5">
+            <Container>
+               <NavbarBrand href="/">shooping list</NavbarBrand>
+               <NavbarToggler onClick={toogle}/>
+               <Collapse isOpen={isOpen} navbar>
+                  <Nav className="ml-auto" navbar>
+                     <NavItem>
+                        <NavLink href="https://github.com">
+                           Github
+                        </NavLink>
+                     </NavItem>
+                  </Nav>
+               </Collapse>
+            </Container>
+         </Navbar>      
+      </div>
+   )
+}
+
+export default AppNavbar
